477a82a354511c39937c7471d71b0429350cd1f6
manero6 <deathworship@hotmail.it>
2023-10-21 19:32:05 +0200

    Add stylish stuff to make things a bit more fancy.
    Also added a mechanism to expand the top message (aka title) to the max
    horizontal size of the terminal (aka columns).

--- updater.sh
+++ updater.sh
@@ -1,5 +1,31 @@
 #! /bin/bash
 
+# This hack is needed to get the correct value for COLUMNS or 'tput cols' when lauching the script on external terminal
+sleep 0.05
+
+declare STYLISH
+   TOP_CHAR=┬
+MIDDLE_CHAR=│
+BOTTOM_CHAR=┴
+TPUT_COLUMNS=$(tput cols)
+#todo LR_OFFSET=3
+
+# Check that 1 charachter is stored in the followng variables, else exit the script
+for CHAR in $TOP_CHAR $MIDDLE_CHAR $BOTTOM_CHAR
+do
+  if ! [ ${#CHAR} -eq 1 ]
+  then
+    echo "TOP_CHAR, MIDDLE_CHAR and BOTTOM_CHAR variables must contain only 1 character!"
+    echo "Please re-check your environment variables or the configuration file."
+    exit 1
+  fi
+done
+
+if which lolcat &>/dev/null
+then
+  STYLISH="| lolcat"
+fi
+
 usage() {
   echo
   echo "Usage:"
@@ -13,6 +39,61 @@ usage() {
   echo "  flatpak-autoremove      Update flatpak packages and remove unused packages"
   echo "  tldr                    Update tldr cache"
   echo
+}
+
+generate_title_top_bottom() {
+  local CHAR=$1
+  eval "for i in {1..$TPUT_COLUMNS}
+        do
+          echo -n "$CHAR"
+        done"
+}
+
+generate_title_middle() {
+  local CHAR=$1
+  local TITLE=$2
+  local TITLE_LENGTH="${#TITLE}"
+
+  eval "for char in {1..$(( ($TPUT_COLUMNS-$TITLE_LENGTH)/2 ))}
+        do
+          echo -n "$CHAR"
+        done"
+}
+
+print_title() {
+  local TITLE=$1
+  local TOP=$(generate_title_top_bottom $TOP_CHAR)
+  # Double quoting TITLE, else the string will be cut at the first whitespace
+  local MIDDLE=$(generate_title_middle $MIDDLE_CHAR "$TITLE")
+  local BOTTOM=$(generate_title_top_bottom $BOTTOM_CHAR)
+
+  local FINAL_TITLE="$MIDDLE $TITLE $MIDDLE"
+  # Cheating a little bit right here... not always in the center but it is not noticeable unless one reads this comment MUHWAHAHA
+  FINAL_TITLE=${FINAL_TITLE::$TPUT_COLUMNS}
+
+  echo $TOP
+  echo $FINAL_TITLE
+  echo $BOTTOM
+}
+
+# old stuff
+#print_title() {
+#  # Dynamically insert '-' based on the total numbers of characters that $1 has
+#  local STRING=$1
+#  local HYPHEN=$(eval "for char in {1..${#1}}
+#                       do
+#                         echo -n -
+#                       done"
+#                )
+#  echo "-----------$HYPHEN--"
+#  echo "| Updating $STRING |"
+#  echo "-----------$HYPHEN--"
+#}
+
+
+print_title_stylish() {
+  # Escaping double quotes is needed, else 'eval' will not preserve whitespaces and do nasty stuff.
+  eval print_title \"$1\" $STYLISH
 }
 
 get_function_command() {
@@ -49,53 +130,41 @@ list_commands() {
 }
 
 all_up() {
-  echo "----------------"
-  echo "| Updating all |"
-  echo "----------------"
-  echo " The following updaters will be executed:"
-  echo "   1) dnf-autoremove"
-  echo "   2) flatpak-autoremove"
-  echo "   3) tldr"
+  print_title_stylish "all"
+  echo "The following updaters will be executed:"
+  echo "  1) dnf-autoremove"
+  echo "  2) flatpak-autoremove"
+  echo "  3) tldr"
   dnf_up_rm
   flatpak_up_rm
   tldr_up
 }
 
 dnf_up() {
-  echo "----------------"
-  echo "| Updating dnf |"
-  echo "----------------"
+  print_title_stylish "dnf"
   # Always refresh cache
   sudo dnf distrosync --refresh
 }
 
 dnf_up_rm() {
-  echo "----------------"
-  echo "| Updating dnf |"
-  echo "----------------"
+  print_title_stylish "dnf & autoremove"
   # Always refresh cache and remove unused packages
   sudo bash -c "dnf distrosync --refresh && dnf autoremove"
 }
 
 flatpak_up() {
-  echo "--------------------"
-  echo "| Updating flatpak |"
-  echo "--------------------"
+  print_title_stylish "flatpak"
   flatpak update
 }
 
 flatpak_up_rm() {
-  echo "--------------------"
-  echo "| Updating flatpak |"
-  echo "--------------------"
+  print_title_stylish "flatpak & autoremove"
   # Uninstall unused packages first
   flatpak uninstall --unused && flatpak update
 }
 
 tldr_up() {
-  echo "-----------------"
-  echo "| Updating tldr |"
-  echo "-----------------"
+  print_title_stylish "tldr"
   tldr -u
 }
 
